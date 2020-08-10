b87617cb2ebcc6e70aba8f73fe520278abf737f2
Aimee Liang <aimeeeliang@gmail.com>
2020-08-10 18:17:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,9 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["Bob", "Linda", "Tina", "Gene"]
-  array << "Louise"
-  return array
+
 end
 
 def add_element_to_start_of_array(array, element)
